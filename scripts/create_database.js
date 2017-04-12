dfb9f63885f422390bf332519730117b1826bbb9
fcl-93 <fabiocl93@gmail.com>
2017-04-12 20:45:54 +0100

    Bower Dependencies. New Database Schema.

--- scripts/create_database.js
+++ scripts/create_database.js
@@ -10,75 +10,75 @@ var connection = mysql.createConnection(dbconfig.connection);
 connection.query('CREATE DATABASE ' + dbconfig.database);
 
 connection.query('\
-CREATE TABLE `' + dbconfig.database + '`.`' + dbconfig.users_table + '` ( \
-    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT, \
-    `username` VARCHAR(20) NOT NULL, \
-    `password` CHAR(60) NOT NULL, \
-    `email` VARCHAR(45) NOT NULL,\
-    `updated_on` DATETIME NOT NULL,\
-        PRIMARY KEY (`id`), \
-    UNIQUE INDEX `id_UNIQUE` (`id` ASC), \
-    UNIQUE INDEX `username_UNIQUE` (`username` ASC) \
-)');
-
-connection.query('\
-    CREATE TABLE IF NOT EXISTS  `' + dbconfig.database + '`.`' + dbconfig.device_table + '` (\
-    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,\
+    CREATE TABLE IF NOT EXISTS `house_db`.`device` (\
+    `id` INT NOT NULL,\
     `device_name` VARCHAR(45) NOT NULL,\
     `updated_on` DATETIME NOT NULL,\
-    PRIMARY KEY (`id`)\
-)');
-
+    PRIMARY KEY (`id`));'
+);
 connection.query('\
-    CREATE TABLE IF NOT EXISTS  `' + dbconfig.database + '`.`' + dbconfig.product_stock_table + '` (\
-    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,\
-    `product_name` VARCHAR(45) NOT NULL,\
-    `weight_default` VARCHAR(45) NOT NULL,\
+    CREATE TABLE IF NOT EXISTS `house_db`.`user` ( \
+    `id` INT NOT NULL,\
+    `username` VARCHAR(45) NOT NULL,\
+    `password` VARCHAR(45) NOT NULL,\
+    `name` VARCHAR(45) NOT NULL,\
+    `email` VARCHAR(45) NOT NULL,\
     `updated_on` DATETIME NOT NULL,\
-    PRIMARY KEY (`id`)\
-)');
+    PRIMARY KEY (`id`));'
+);
 connection.query('\
-    CREATE TABLE IF NOT EXISTS `house_db`.`device_has_users` (\
-    `device_id` INT UNSIGNED NOT NULL,\
-    `users_id` INT UNSIGNED NOT NULL,\
-    PRIMARY KEY (`device_id`, `users_id`),\
-    INDEX `fk_device_has_users_users1_idx` (`users_id` ASC),\
-    CONSTRAINT `fk_device_has_users_device1`\
-        FOREIGN KEY (`device_id`)\
-        REFERENCES `house_db`.`device` (`id`)\
-        ON DELETE NO ACTION\
-        ON UPDATE NO ACTION,\
-    CONSTRAINT `fk_device_has_users_users1`\
-        FOREIGN KEY (`users_id`)\
-        REFERENCES `house_db`.`users` (`id`)\
-        ON DELETE NO ACTION\
-        ON UPDATE NO ACTION)\
-');
-
-connection.query('\
-    CREATE TABLE IF NOT EXISTS `house_db`.`device_has_product_stock` (\
-    `stock` INT NOT NULL,\
-    `stocked_weight` FLOAT NOT NULL,\
-    `expiration_date` DATE NOT NULL,\
+    CREATE TABLE IF NOT EXISTS `house_db`.`product` (\
+    `id` INT UNSIGNED NOT NULL,\
+    `name` VARCHAR(255) NOT NULL,\
+    `default_weight` FLOAT NOT NULL,\
+    `updated_on` DATETIME NOT NULL,\
     `state` ENUM("DISABLE", "TOBUY", "ONSTOCK") NOT NULL,\
-    `previous_weight` FLOAT NOT NULL,\
-    `product_stock_id` INT UNSIGNED NOT NULL,\
-    `device_id` INT UNSIGNED NOT NULL,\
-    INDEX `fk_device_has_product_stock_product_stock1_idx` (`product_stock_id` ASC),\
-    INDEX `fk_device_has_product_stock_device1_idx` (`device_id` ASC),\
-    PRIMARY KEY (`product_stock_id`, `device_id`),\
-    CONSTRAINT `fk_device_has_product_stock_product_stock1`\
-        FOREIGN KEY (`product_stock_id`)\
-        REFERENCES `house_db`.`product_stock` (`id`)\
+    `description` VARCHAR(255) NOT NULL,\
+    PRIMARY KEY (`id`),\
+    UNIQUE INDEX `id_UNIQUE` (`id` ASC));\
+    ');
+connection.query('\
+    CREATE TABLE IF NOT EXISTS `house_db`.`device_has_user` (\
+    `device_id` INT NOT NULL,\
+    `user_id` INT NOT NULL,\
+    PRIMARY KEY (`device_id`, `user_id`),\
+    INDEX `fk_device_has_user_user1_idx` (`user_id` ASC),\
+    INDEX `fk_device_has_user_device1_idx` (`device_id` ASC),\
+    CONSTRAINT `fk_device_has_user_device1`\
+        FOREIGN KEY (`device_id`)\
+        REFERENCES `house_db`.`device` (`id`)\
         ON DELETE NO ACTION\
         ON UPDATE NO ACTION,\
-    CONSTRAINT `fk_device_has_product_stock_device1`\
+    CONSTRAINT `fk_device_has_user_user1`\
+        FOREIGN KEY (`user_id`)\
+        REFERENCES `house_db`.`user` (`id`)\
+        ON DELETE NO ACTION\
+        ON UPDATE NO ACTION);\
+');
+connection.query('\
+    CREATE TABLE IF NOT EXISTS `house_db`.`product_item` (\
+    `id` INT NOT NULL,\
+    `product_id` INT UNSIGNED NOT NULL,\
+    `device_id` INT NOT NULL,\
+    `actual_weight` FLOAT NOT NULL,\
+    `expiration_date` DATE NOT NULL,\
+    `previous_weight` FLOAT NOT NULL,\
+    `updated_on` DATETIME NOT NULL,\
+    PRIMARY KEY (`id`),\
+    UNIQUE INDEX `id_UNIQUE` (`id` ASC),\
+    INDEX `fk_product_item_product1_idx` (`product_id` ASC),\
+    INDEX `fk_product_item_device1_idx` (`device_id` ASC),\
+    CONSTRAINT `fk_product_item_product1`\
+        FOREIGN KEY (`product_id`)\
+        REFERENCES `house_db`.`product` (`id`)\
+        ON DELETE NO ACTION\
+        ON UPDATE NO ACTION,\
+    CONSTRAINT `fk_product_item_device1`\
         FOREIGN KEY (`device_id`)\
         REFERENCES `house_db`.`device` (`id`)\
         ON DELETE NO ACTION\
         ON UPDATE NO ACTION)\
 ');
-
 console.log('Success: Database Created!');
 
 connection.end();
